83ec75da1374d810951d207c7025ac0621ec6043
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-08-28 11:51:45 -0400

    Created form for signup modal

--- src/components/modal/SignUp.js
+++ src/components/modal/SignUp.js
@@ -1,5 +1,4 @@
-import '../../styles/style.css';
-import '../../styles/modal.css';
+import { useState } from 'react';
 
 
 function SignUp(props) {
@@ -13,6 +12,22 @@ function SignUp(props) {
           </div>
           <button id='close-modal' onClick={props.toggleModal}>X</button>
         </div>
+        <hr />
+        <form className='signup-form'>
+          <div id='form-names'>
+            <label htmlFor='first_name'>First name</label>
+            <input type='text' id='first_name' name='first_name' placeholder='First name' />
+            <label htmlFor='last_name'>First name</label>
+            <input type='text' id='last_name' name='last_name' placeholder='Last name' />
+          </div>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email' name='email' placeholder='Email' />
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' name='password' placeHolder='Password' />
+
+          <button type='submit' className='button-style'>Sign Up</button>
+
+        </form>
       </div>
     </div>
   );
