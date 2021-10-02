57c87f02b607a281d04dd02defd1f16e5ff769cf
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-10-02 12:23:20 -0400

    Added validation for the signup form

--- src/components/modal/SignUp.js
+++ src/components/modal/SignUp.js
@@ -1,6 +1,14 @@
 import { useState } from 'react';
 
 
+/*
+Thought process
+Password is the only field that needs validation - ensure it is eight characters
+
+
+*/
+
+
 function SignUp(props) {
 
   // State for the form contents
@@ -12,26 +20,38 @@ function SignUp(props) {
   })
 
   // State to hold the errors
-  const [errors, setErrors] = useState({
-    first_name: '',
-    last_name: '',
-    email: '',
-    password: ''
-  })
+  const [passError, setPassError] = useState('');
+
+  const passValidate = (password) => {
+    let numTest = new RegExp('.*[0-9]');
+    let symTest = new RegExp('.*[!@#$%^&*]');
+
+    if (password.length < 8 || !numTest.test(password) || !symTest.test(password)) {
+      setPassError('Password must have 8 characters, 1 number, and 1 symbol')
+      return false;
+    } else {
+      setPassError('')
+      return true;
+    }
+  }
+
 
   const handleChange = (e) => {
     setNewUser(prevState => ({
       ...prevState,
       [e.target.id]: e.target.value
     }))
-
-    console.log(newUser)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('would submit')
+
+    let passed = passValidate(newUser.password)
+    console.log(`passed: ${passed}`)
     console.log(newUser)
+
+    // Validation is done. Now we know that we'd have all the data we need,
+    // so. We can now submit the data to the server
   }
 
   return (
@@ -48,7 +68,7 @@ function SignUp(props) {
         <form className='signup-form' onSubmit={handleSubmit}>
           <div id='form-names'>
             <label htmlFor='first_name'>First name</label>
-            <input type='text' id='first_name' name='first_name' placeholder='First name' required onChange={handleChange} initialvalue={newUser.first_name} value={newUser.first_name}/>
+            <input type='text' id='first_name' name='first_name' placeholder='First name' autoFocus required onChange={handleChange} initialvalue={newUser.first_name} value={newUser.first_name}/>
             <label htmlFor='last_name'>First name</label>
             <input type='text' id='last_name' name='last_name' placeholder='Last name' required onChange={handleChange} initialvalue={newUser.last_name} value={newUser.last_name}/>
           </div>
@@ -56,7 +76,8 @@ function SignUp(props) {
           <input type='email' id='email' name='email' placeholder='Email' required onChange={handleChange} initialvalue={newUser.email} value={newUser.email}/>
           <label htmlFor='password'>Password</label>
           <input type='password' id='password' name='password' placeholder='Password' required onChange={handleChange} initialvalue={newUser.password} value={newUser.password}/>
-
+          {/* {passError.length !== 0? } */}
+          <span className='error'>{passError.length !== 0? passError : ''}</span>
           <button type='submit' className='button-style'>Sign Up</button>
 
         </form>
